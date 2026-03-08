6213d8171f77f87c401a2bb0d59ea118a96e0a64
fruestueck <code@msilber.at>
2026-03-08 15:02:12 +0100

    check array_key_last instead of next($array)

--- app/Parser.php
+++ app/Parser.php
@@ -38,18 +38,20 @@ final class Parser
         // write
         $fo = fopen($outputPath, "w");
         $buffer = '{'.PHP_EOL;
+        $lastPath = array_key_last($map);
         foreach($map as $path => $dates) {
             $buffer .= "    \"$blog$path\": {".PHP_EOL;
 
             ksort($dates);
 
+            $lastDate = array_key_last($dates);
             foreach($dates as $date => $visits) {
                 $buffer .= "        \"".
                     substr($date, 0, 4) . '-' . substr($date, 4, 2) . '-' . substr($date, 6, 2)
-                    ."\": $visits".(next($dates) !== false ? ',' : '').PHP_EOL;
+                    ."\": $visits".($date === $lastDate ? '' : ',').PHP_EOL;
             }
 
-            $buffer.= "    }".(next($map) !== false ? ',' : '').PHP_EOL;
+            $buffer.= "    }".($path === $lastPath ? '' : ',').PHP_EOL;
 
             if(strlen($buffer) > 1_000) {
                 fwrite($fo, $buffer);
